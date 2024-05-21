f04d6e56a81761d6bab16cc13947d871111a0330
Chems Hoflack <chhoflac@k1r3p9.42mulhouse.fr>
2024-05-21 14:32:46 +0200

    ajouts de files

---
+++ philo/philo.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/21 13:27:09 by chhoflac          #+#    #+#             */
+/*   Updated: 2024/05/21 14:32:01 by chhoflac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+
+
+#endif
+
