4c5ecd378a011640a95d1c73694c7a5654ad014a
Chems Hoflack <chhoflac@k1r3p9.42mulhouse.fr>
2024-06-02 16:14:02 +0200

    premier jet

--- philo/sources/main.c
+++ philo/sources/main.c
@@ -6,11 +6,33 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 13:07:06 by chhoflac          #+#    #+#             */
-/*   Updated: 2024/06/02 13:51:47 by chhoflac         ###   ########.fr       */
+/*   Updated: 2024/06/02 16:10:18 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void	*eat(void *test)
+{
+	test = (int *) test;
+	printf("time_to_eat\n");
+	return(NULL);
+}
+
+void	try(t_table *table)
+{
+	int		i;
+	void	*test;
+
+	test = NULL;
+	i = 0;
+	while (i < table->nb_philo)
+	{
+		pthread_create(&table->philos[i], NULL, eat, test);
+		pthread_join(table->philos[i], NULL);
+		i++;
+	}
+}
 
 int	main(int argc, char **argv)
 {
@@ -19,8 +41,6 @@ int	main(int argc, char **argv)
 	if (argc >= 5 && argc <= 6)
 	{
 		table = create_table(argc, argv);
-		printf("%d %d %llu %llu %llu %d", table->nb_forks, table->nb_philo,
-			table->time_to_die, table->time_to_eat, table->time_to_sleep,
-			table->must_eat);
+		try(table);
 	}
 }
