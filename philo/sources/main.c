97450ef3a8e467e1e67ab4c58f8f58534eb16947
Chems Hoflack <chhoflac@k1r3p9.42mulhouse.fr>
2024-06-12 14:41:08 +0200

    ajout de ft_free

--- philo/sources/main.c
+++ philo/sources/main.c
@@ -6,20 +6,26 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 13:07:06 by chhoflac          #+#    #+#             */
-/*   Updated: 2024/06/02 16:10:18 by chhoflac         ###   ########.fr       */
+/*   Updated: 2024/06/12 14:40:29 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	*eat(void *test)
+void	ft_free(t_table *table)
+{
+	free(table->philos);
+	free(table);
+}
+
+void	*routine(void *test)
 {
 	test = (int *) test;
 	printf("time_to_eat\n");
-	return(NULL);
+	return (NULL);
 }
 
-void	try(t_table *table)
+void	go(t_table *table)
 {
 	int		i;
 	void	*test;
@@ -28,7 +34,7 @@ void	try(t_table *table)
 	i = 0;
 	while (i < table->nb_philo)
 	{
-		pthread_create(&table->philos[i], NULL, eat, test);
+		pthread_create(&table->philos[i], NULL,	routine, table);
 		pthread_join(table->philos[i], NULL);
 		i++;
 	}
@@ -41,6 +47,7 @@ int	main(int argc, char **argv)
 	if (argc >= 5 && argc <= 6)
 	{
 		table = create_table(argc, argv);
-		try(table);
+		go(table);
+		ft_free(table);
 	}
 }
